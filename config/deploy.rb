f92cdbcd7f6bf81e1f01e3d67c3ea8d58cb78d9c
Maycon Ribeiro <maycon_ribeiro@hotmail.com>
2018-11-17 20:26:35 -0200

    deploy unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,6 +40,8 @@ set :keep_releases, 5
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
+set :rvm_map_bins, %w{gem rake ruby rails bundle}
+
 after 'deploy:publishing', 'deploy:restart'
 
 namespace :deploy do
