7a734cf2ae0f62b985dc7434e209dbfdde6ef311
Maycon Ribeiro <maycon_ribeiro@hotmail.com>
2018-11-17 20:39:45 -0200

    config unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
-set :rvm_ruby_version, 'ruby-2.5.1-p57'
+set :rvm_ruby_version, 'ruby-2.5.1'
 
 after 'deploy:publishing', 'deploy:restart'
 
