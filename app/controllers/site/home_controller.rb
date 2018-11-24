664b4483152a348ce1726d230a4bbe5e498850f1
Maycon Ribeiro <maycon_ribeiro@hotmail.com>
2018-11-24 20:01:26 -0200

    cookies rails

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -1,8 +1,19 @@
 class Site::HomeController < SiteController
 
   def index
+
+    cookies[:user_name] = "david"
+    cookies[:lat_lon] = JSON.generate([47.68, -122.37])
+    cookies[:login] = { value: "XJ-122", expires: 1.hour.from_now }
+    cookies.signed[:user_id] = "Member Maycon"
+    cookies.encrypted[:discount] = "EscamboApp"
+
     @categories = Category.order_by_description
     @ads = Ad.descending_order(params[:page])
     @carousel = Ad.carousel_random()
+
+    puts ">>>>>>>>> #{cookies[:user_name]}"
+    puts ">>>>>>>>> #{cookies[:lat_lon]}"
+    puts ">>>>>>>>> #{eval(cookies[:lat_lon]).class}"
   end
 end
--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -3,5 +3,7 @@ class Site::SearchController < SiteController
   def ads
     @categories = Category.order_by_description
     @ads = Ad.search(params[:q], params[:page])
+
+    cookies[:categories] = @categories.to_json
   end
 end
